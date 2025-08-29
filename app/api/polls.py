ca4c027d2472273a41b6763949283eb17856a40d
haideptrai99 <haidaica99999@gmail.com>
2025-08-29 21:37:58 +0700

    config api router

--- app/api/polls.py
+++ app/api/polls.py
@@ -1,3 +1,26 @@
-from fastapi import APIRouter
+from typing import Any
+from uuid import UUID
+
+from fastapi import APIRouter, HTTPException
+
+from app.models.Polls import Poll, PollCreate
+from app.services import utils
 
 router = APIRouter()
+
+
+# @app.post("/polls/create")
+@router.post("/create")
+def create_poll(poll: PollCreate) -> dict[str, Any]:
+    new_poll = poll.create_poll()
+    utils.save_poll(new_poll)
+    return {"detail": "Poll sucesss created", "poll_id": new_poll.id, "poll": new_poll}
+
+
+# @app.get("/polls/{poll_id}", response_model=Poll)
+@router.post("/{poll_id}")
+def get_poll(poll_id: UUID) -> Poll:
+    poll = utils.get_poll(poll_id)
+    if not poll:
+        raise HTTPException(status_code=400, detail="A poll id not correct")
+    return poll
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from typing import Any
-from uuid import UUID
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 
-from app.models.Polls import Poll, PollCreate
-from app.services import utils
+from app.api import polls
 
 app = FastAPI()
+
+app.include_router(polls.router, prefix="/polls", tags=["polls"])
 
 
 class Message(BaseModel):
@@ -22,18 +20,3 @@ class Message(BaseModel):
 )
 def read_root() -> Message:
     return Message(message="Hello, World!")
-
-
-@app.post("/polls/create")
-def create_poll(poll: PollCreate) -> dict[str, Any]:
-    new_poll = poll.create_poll()
-    utils.save_poll(new_poll)
-    return {"detail": "Poll sucesss created", "poll_id": new_poll.id, "poll": new_poll}
-
-
-@app.get("/polls/{poll_id}", response_model=Poll)
-def get_poll(poll_id: UUID) -> Poll:
-    poll = utils.get_poll(poll_id)
-    if not poll:
-        raise HTTPException(status_code=400, detail="A poll id not correct")
-    return poll
