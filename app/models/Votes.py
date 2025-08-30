de25b0857fa594a32f7531ceb45d68d12903deb8
haideptrai99 <haidaica99999@gmail.com>
2025-08-30 10:26:22 +0700

    add Votes modal

---
+++ app/models/Votes.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class VoterCreate(BaseModel):
+    """The Voter write model, consisting of only email address"""
+
+    email: EmailStr
+
+
+class Voter(VoterCreate):
+    """The Voter read model"""
+
+    voted_at: datetime = Field(default_factory=datetime.now)
+
+
+class Vote(BaseModel):
+    """The Vote read model"""
+
+    poll_id: UUID
+    choice_id: UUID
+    voter: Voter
+
+
+class VoteById(BaseModel):
+    choice_id: UUID
+    voter: VoterCreate
+
+
+class VoteByLabel(BaseModel):
+    choice_label: int
+    voter: VoterCreate
