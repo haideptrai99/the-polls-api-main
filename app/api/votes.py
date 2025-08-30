ba62e3a73bfb5dd8e8aa6fc1ebf6e7188785632e
haideptrai99 <haidaica99999@gmail.com>
2025-08-30 22:01:42 +0700

    config api vote

--- app/api/votes.py
+++ app/api/votes.py
@@ -1,17 +1,36 @@
+from typing import Any
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from app.models.Votes import VoteById, VoteByLabel
+from app.models.Votes import Vote, VoteById, VoteByLabel, Voter
+from app.services import utils
 
 router = APIRouter()
 
 
 @router.post("/{poll_id}/id")
-def vote_by_id(poll_id: UUID, vote: VoteById) -> dict[str, str]:
-    return {"message": "Vote recorded"}
+def vote_by_id(poll_id: UUID, vote: VoteById) -> dict[str, Any]:
+    vote_model = Vote(
+        poll_id=poll_id,
+        choice_id=vote.choice_id,
+        voter=Voter(**vote.voter.model_dump()),
+    )
+
+    return {"message": "Vote recorded", "vote": vote_model}
 
 
 @router.post("/{poll_id}/label")
-def vote_by_label(poll_id: UUID, vote: VoteByLabel) -> dict[str, str]:
-    return {"message": "Vote recorded"}
+def vote_by_label(poll_id: UUID, vote: VoteByLabel) -> dict[str, Any]:
+    choice_id = utils.get_choice_id_by_label(poll_id, vote.choice_label)
+
+    if not choice_id:
+        raise HTTPException(status_code=400, detail="Invalid choice label provided.")
+
+    vote_model = Vote(
+        poll_id=poll_id,
+        choice_id=choice_id,
+        voter=Voter(**vote.voter.model_dump()),
+    )
+
+    return {"message": "Vote recorded", "vote": vote_model}
